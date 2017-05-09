ff59b1ddfdc539ffa15fc41c89bb5ad48ad2a427
Tyler Gunter <tyler.james.gunter@gmail.com>
2017-05-08 20:26:02 -0400

    finish dragon game

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 var slaying = true;
 
 while (slaying) {
-    if (youHit === true) {
+    if (youHit) {
         console.log("You hit the dragon!");
+        totalDamage += damageThisRound;
+        if (totalDamage >= 4) {
+            console.log("You slayed the Dragon!");
+             slaying = false;
+        } else {
+            youHit = Math.floor(Math.random() * 2);
+        }
     } else {
         console.log("The dragon defeated you!");
+        slaying = false;
     }
-    slaying = false;
 }
 
 var youHit = function () {
